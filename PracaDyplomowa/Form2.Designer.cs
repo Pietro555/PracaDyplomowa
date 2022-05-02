027fe7149d0f0ab894395776746bb21163ba402f
Pietro555 <aseba8@wp.pl>
2022-05-02 17:11:51 +0200

    Working Add buttons in Form2

--- PracaDyplomowa/Form2.Designer.cs
+++ PracaDyplomowa/Form2.Designer.cs
@@ -30,29 +30,48 @@ namespace PracaDyplomowa
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.buttonDodajProcek = new System.Windows.Forms.Button();
+            this.comboBoxDysk = new System.Windows.Forms.ComboBox();
+            this.labelDysk = new System.Windows.Forms.Label();
+            this.comboBoxRAM = new System.Windows.Forms.ComboBox();
+            this.labelRAM = new System.Windows.Forms.Label();
+            this.comboBoxGrafa = new System.Windows.Forms.ComboBox();
+            this.labelKartaGraficzna = new System.Windows.Forms.Label();
+            this.comboBoxProcesor = new System.Windows.Forms.ComboBox();
             this.labelProcesor = new System.Windows.Forms.Label();
             this.PictureBoxComputerBySide = new System.Windows.Forms.PictureBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.labelKartaGraficzna = new System.Windows.Forms.Label();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
-            this.labelRAM = new System.Windows.Forms.Label();
-            this.comboBox4 = new System.Windows.Forms.ComboBox();
-            this.labelDysk = new System.Windows.Forms.Label();
+            this.pictureBoxProcesor = new System.Windows.Forms.PictureBox();
+            this.pictureBoxKartaGraf = new System.Windows.Forms.PictureBox();
+            this.pictureBoxDysk = new System.Windows.Forms.PictureBox();
+            this.pictureBoxRam = new System.Windows.Forms.PictureBox();
+            this.buttonDodajKarteGraf = new System.Windows.Forms.Button();
+            this.buttonDodajRam = new System.Windows.Forms.Button();
+            this.buttonDodajDysk = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.PictureBoxComputerBySide)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProcesor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxKartaGraf)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDysk)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRam)).BeginInit();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Transparent;
-            this.panel1.Controls.Add(this.comboBox4);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.buttonDodajDysk);
+            this.panel1.Controls.Add(this.buttonDodajRam);
+            this.panel1.Controls.Add(this.buttonDodajKarteGraf);
+            this.panel1.Controls.Add(this.buttonDodajProcek);
+            this.panel1.Controls.Add(this.comboBoxDysk);
             this.panel1.Controls.Add(this.labelDysk);
-            this.panel1.Controls.Add(this.comboBox3);
+            this.panel1.Controls.Add(this.comboBoxRAM);
             this.panel1.Controls.Add(this.labelRAM);
-            this.panel1.Controls.Add(this.comboBox2);
+            this.panel1.Controls.Add(this.comboBoxGrafa);
             this.panel1.Controls.Add(this.labelKartaGraficzna);
-            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.comboBoxProcesor);
             this.panel1.Controls.Add(this.labelProcesor);
             this.panel1.Controls.Add(this.PictureBoxComputerBySide);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -61,13 +80,74 @@ namespace PracaDyplomowa
             this.panel1.Size = new System.Drawing.Size(944, 651);
             this.panel1.TabIndex = 0;
             // 
-            // comboBox1
+            // buttonDodajProcek
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(707, 25);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(160, 21);
-            this.comboBox1.TabIndex = 2;
+            this.buttonDodajProcek.Location = new System.Drawing.Point(821, 37);
+            this.buttonDodajProcek.Name = "buttonDodajProcek";
+            this.buttonDodajProcek.Size = new System.Drawing.Size(96, 24);
+            this.buttonDodajProcek.TabIndex = 9;
+            this.buttonDodajProcek.Text = "Dodaj";
+            this.buttonDodajProcek.UseVisualStyleBackColor = true;
+            this.buttonDodajProcek.Click += new System.EventHandler(this.buttonDodajProcek_Click);
+            // 
+            // comboBoxDysk
+            // 
+            this.comboBoxDysk.FormattingEnabled = true;
+            this.comboBoxDysk.Location = new System.Drawing.Point(636, 372);
+            this.comboBoxDysk.Name = "comboBoxDysk";
+            this.comboBoxDysk.Size = new System.Drawing.Size(160, 21);
+            this.comboBoxDysk.TabIndex = 8;
+            // 
+            // labelDysk
+            // 
+            this.labelDysk.AutoSize = true;
+            this.labelDysk.Location = new System.Drawing.Point(750, 343);
+            this.labelDysk.Name = "labelDysk";
+            this.labelDysk.Size = new System.Drawing.Size(71, 13);
+            this.labelDysk.TabIndex = 7;
+            this.labelDysk.Text = "Dysk Pamięci";
+            // 
+            // comboBoxRAM
+            // 
+            this.comboBoxRAM.FormattingEnabled = true;
+            this.comboBoxRAM.Location = new System.Drawing.Point(636, 239);
+            this.comboBoxRAM.Name = "comboBoxRAM";
+            this.comboBoxRAM.Size = new System.Drawing.Size(160, 21);
+            this.comboBoxRAM.TabIndex = 6;
+            // 
+            // labelRAM
+            // 
+            this.labelRAM.AutoSize = true;
+            this.labelRAM.Location = new System.Drawing.Point(765, 223);
+            this.labelRAM.Name = "labelRAM";
+            this.labelRAM.Size = new System.Drawing.Size(31, 13);
+            this.labelRAM.TabIndex = 5;
+            this.labelRAM.Text = "RAM";
+            // 
+            // comboBoxGrafa
+            // 
+            this.comboBoxGrafa.FormattingEnabled = true;
+            this.comboBoxGrafa.Location = new System.Drawing.Point(636, 129);
+            this.comboBoxGrafa.Name = "comboBoxGrafa";
+            this.comboBoxGrafa.Size = new System.Drawing.Size(160, 21);
+            this.comboBoxGrafa.TabIndex = 4;
+            // 
+            // labelKartaGraficzna
+            // 
+            this.labelKartaGraficzna.AutoSize = true;
+            this.labelKartaGraficzna.Location = new System.Drawing.Point(743, 113);
+            this.labelKartaGraficzna.Name = "labelKartaGraficzna";
+            this.labelKartaGraficzna.Size = new System.Drawing.Size(78, 13);
+            this.labelKartaGraficzna.TabIndex = 3;
+            this.labelKartaGraficzna.Text = "Karta graficzna";
+            // 
+            // comboBoxProcesor
+            // 
+            this.comboBoxProcesor.FormattingEnabled = true;
+            this.comboBoxProcesor.Location = new System.Drawing.Point(636, 37);
+            this.comboBoxProcesor.Name = "comboBoxProcesor";
+            this.comboBoxProcesor.Size = new System.Drawing.Size(160, 21);
+            this.comboBoxProcesor.TabIndex = 2;
             // 
             // labelProcesor
             // 
@@ -88,56 +168,83 @@ namespace PracaDyplomowa
             this.PictureBoxComputerBySide.TabIndex = 0;
             this.PictureBoxComputerBySide.TabStop = false;
             // 
-            // comboBox2
+            // pictureBoxProcesor
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(707, 93);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(160, 21);
-            this.comboBox2.TabIndex = 4;
+            this.pictureBoxProcesor.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxProcesor.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxProcesor.Name = "pictureBoxProcesor";
+            this.pictureBoxProcesor.Size = new System.Drawing.Size(255, 226);
+            this.pictureBoxProcesor.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxProcesor.TabIndex = 10;
+            this.pictureBoxProcesor.TabStop = false;
             // 
-            // labelKartaGraficzna
+            // pictureBoxKartaGraf
             // 
-            this.labelKartaGraficzna.AutoSize = true;
-            this.labelKartaGraficzna.Location = new System.Drawing.Point(743, 77);
-            this.labelKartaGraficzna.Name = "labelKartaGraficzna";
-            this.labelKartaGraficzna.Size = new System.Drawing.Size(78, 13);
-            this.labelKartaGraficzna.TabIndex = 3;
-            this.labelKartaGraficzna.Text = "Karta graficzna";
+            this.pictureBoxKartaGraf.Location = new System.Drawing.Point(0, 292);
+            this.pictureBoxKartaGraf.Name = "pictureBoxKartaGraf";
+            this.pictureBoxKartaGraf.Size = new System.Drawing.Size(255, 214);
+            this.pictureBoxKartaGraf.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxKartaGraf.TabIndex = 11;
+            this.pictureBoxKartaGraf.TabStop = false;
             // 
-            // comboBox3
+            // pictureBoxDysk
             // 
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(707, 151);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(160, 21);
-            this.comboBox3.TabIndex = 6;
+            this.pictureBoxDysk.Location = new System.Drawing.Point(252, 0);
+            this.pictureBoxDysk.Name = "pictureBoxDysk";
+            this.pictureBoxDysk.Size = new System.Drawing.Size(234, 226);
+            this.pictureBoxDysk.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxDysk.TabIndex = 12;
+            this.pictureBoxDysk.TabStop = false;
             // 
-            // labelRAM
+            // pictureBoxRam
             // 
-            this.labelRAM.AutoSize = true;
-            this.labelRAM.Location = new System.Drawing.Point(765, 135);
-            this.labelRAM.Name = "labelRAM";
-            this.labelRAM.Size = new System.Drawing.Size(31, 13);
-            this.labelRAM.TabIndex = 5;
-            this.labelRAM.Text = "RAM";
+            this.pictureBoxRam.Location = new System.Drawing.Point(252, 292);
+            this.pictureBoxRam.Name = "pictureBoxRam";
+            this.pictureBoxRam.Size = new System.Drawing.Size(234, 214);
+            this.pictureBoxRam.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxRam.TabIndex = 13;
+            this.pictureBoxRam.TabStop = false;
             // 
-            // comboBox4
+            // buttonDodajKarteGraf
             // 
-            this.comboBox4.FormattingEnabled = true;
-            this.comboBox4.Location = new System.Drawing.Point(707, 208);
-            this.comboBox4.Name = "comboBox4";
-            this.comboBox4.Size = new System.Drawing.Size(160, 21);
-            this.comboBox4.TabIndex = 8;
+            this.buttonDodajKarteGraf.Location = new System.Drawing.Point(821, 126);
+            this.buttonDodajKarteGraf.Name = "buttonDodajKarteGraf";
+            this.buttonDodajKarteGraf.Size = new System.Drawing.Size(96, 24);
+            this.buttonDodajKarteGraf.TabIndex = 14;
+            this.buttonDodajKarteGraf.Text = "Dodaj";
+            this.buttonDodajKarteGraf.UseVisualStyleBackColor = true;
+            this.buttonDodajKarteGraf.Click += new System.EventHandler(this.buttonDodajKarteGraf_Click);
             // 
-            // labelDysk
+            // buttonDodajRam
             // 
-            this.labelDysk.AutoSize = true;
-            this.labelDysk.Location = new System.Drawing.Point(743, 192);
-            this.labelDysk.Name = "labelDysk";
-            this.labelDysk.Size = new System.Drawing.Size(71, 13);
-            this.labelDysk.TabIndex = 7;
-            this.labelDysk.Text = "Dysk Pamięci";
+            this.buttonDodajRam.Location = new System.Drawing.Point(821, 236);
+            this.buttonDodajRam.Name = "buttonDodajRam";
+            this.buttonDodajRam.Size = new System.Drawing.Size(96, 24);
+            this.buttonDodajRam.TabIndex = 15;
+            this.buttonDodajRam.Text = "Dodaj";
+            this.buttonDodajRam.UseVisualStyleBackColor = true;
+            this.buttonDodajRam.Click += new System.EventHandler(this.buttonDodajRam_Click);
+            // 
+            // buttonDodajDysk
+            // 
+            this.buttonDodajDysk.Location = new System.Drawing.Point(821, 369);
+            this.buttonDodajDysk.Name = "buttonDodajDysk";
+            this.buttonDodajDysk.Size = new System.Drawing.Size(96, 24);
+            this.buttonDodajDysk.TabIndex = 16;
+            this.buttonDodajDysk.Text = "Dodaj";
+            this.buttonDodajDysk.UseVisualStyleBackColor = true;
+            this.buttonDodajDysk.Click += new System.EventHandler(this.buttonDodajDysk_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pictureBoxProcesor);
+            this.panel2.Controls.Add(this.pictureBoxRam);
+            this.panel2.Controls.Add(this.pictureBoxDysk);
+            this.panel2.Controls.Add(this.pictureBoxKartaGraf);
+            this.panel2.Location = new System.Drawing.Point(36, 51);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(486, 544);
+            this.panel2.TabIndex = 17;
             // 
             // Form2
             // 
@@ -153,6 +260,11 @@ namespace PracaDyplomowa
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.PictureBoxComputerBySide)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProcesor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxKartaGraf)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDysk)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRam)).EndInit();
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -162,12 +274,21 @@ namespace PracaDyplomowa
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.PictureBox PictureBoxComputerBySide;
         private System.Windows.Forms.Label labelProcesor;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBoxProcesor;
+        private System.Windows.Forms.ComboBox comboBoxDysk;
         private System.Windows.Forms.Label labelDysk;
-        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBoxRAM;
         private System.Windows.Forms.Label labelRAM;
-        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBoxGrafa;
         private System.Windows.Forms.Label labelKartaGraficzna;
+        private System.Windows.Forms.Button buttonDodajProcek;
+        private System.Windows.Forms.PictureBox pictureBoxRam;
+        private System.Windows.Forms.PictureBox pictureBoxDysk;
+        private System.Windows.Forms.PictureBox pictureBoxKartaGraf;
+        private System.Windows.Forms.PictureBox pictureBoxProcesor;
+        private System.Windows.Forms.Button buttonDodajDysk;
+        private System.Windows.Forms.Button buttonDodajRam;
+        private System.Windows.Forms.Button buttonDodajKarteGraf;
+        private System.Windows.Forms.Panel panel2;
     }
 }
--- PracaDyplomowa/Form2.cs
+++ PracaDyplomowa/Form2.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,10 +14,66 @@ namespace PracaDyplomowa
 {
     public partial class Form2 : Form
     {
+        List<Component> procesory = new List<Component>();
+        List<Component> kartyGraficzne = new List<Component>();
+        List<Component> dyski = new List<Component>();
+        List<Component> ramy = new List<Component>();
+
         public Form2()
         {
             InitializeComponent();
             this.Paint += new PaintEventHandler(GradientBackground);
+
+
+            Component intelCore = new Component("Intel core", "Specyfikacje intel cora", "Intel",
+                Image.FromFile(@"D:\Visual Projects\PracaDyplomowa\PracaDyplomowa\Properties\Images\intelcore.png"));
+            Component amdRyzen = new Component("AMD ryzen", "Specyfikacje AMD ryzena", "AMD", 
+                Image.FromFile(@"D:\Visual Projects\PracaDyplomowa\PracaDyplomowa\Properties\Images\amdRyzen.png"));
+            procesory.Add(intelCore);
+            procesory.Add(amdRyzen);
+
+            foreach(Component c in procesory)
+            {
+                comboBoxProcesor.Items.Add(c.ToString());
+            }
+
+            Component geforceRTX = new Component("RTX", "Specyfikacje RTXa", "Nvidia", 
+                Image.FromFile(@"D:\Visual Projects\PracaDyplomowa\PracaDyplomowa\Properties\Images\rtx.png"));
+            Component AmdRadeon = new Component("AMD radeon", "Specyfikacje radeona", "AMD", 
+                Image.FromFile(@"D:\Visual Projects\PracaDyplomowa\PracaDyplomowa\Properties\Images\radeon.png"));
+            kartyGraficzne.Add(geforceRTX);
+            kartyGraficzne.Add(AmdRadeon);
+
+            foreach (Component c in kartyGraficzne)
+            {
+                comboBoxGrafa.Items.Add(c.ToString());
+            }
+
+            Component hdd = new Component("HDD", "Specyfikacje HDD", "Toshiba", 
+                Image.FromFile(@"D:\Visual Projects\PracaDyplomowa\PracaDyplomowa\Properties\Images\hdd.png"));
+            Component ssd = new Component("SSD", "Specyfikacje SSD", "Western Digital", 
+                Image.FromFile(@"D:\Visual Projects\PracaDyplomowa\PracaDyplomowa\Properties\Images\ssd.png"));
+            dyski.Add(hdd);
+            dyski.Add(ssd);
+
+            foreach (Component c in dyski)
+            {
+                comboBoxDysk.Items.Add(c.ToString());
+            }
+
+            Component furyBeast = new Component("RAM Fury Beast", "Specyfikacje fury beast", "Kingston", 
+                Image.FromFile(@"D:\Visual Projects\PracaDyplomowa\PracaDyplomowa\Properties\Images\ramfury.png"));
+            Component vengance = new Component("RAM Vengeance", "Specyfikacje vengena", "Corsair", 
+                Image.FromFile(@"D:\Visual Projects\PracaDyplomowa\PracaDyplomowa\Properties\Images\vegence.png"));
+            ramy.Add(furyBeast);
+            ramy.Add(vengance);
+
+            foreach(Component c in ramy)
+            {
+                comboBoxRAM.Items.Add(c.ToString());
+            }
+
+
 
             this.Invalidate();
         }
@@ -25,11 +82,34 @@ namespace PracaDyplomowa
         {
             Graphics g = e.Graphics;
             Rectangle gradientRect = new Rectangle(0, 0, Width, Height);
-            Brush b = new LinearGradientBrush(gradientRect, Color.FromArgb(0, 214, 136), Color.FromArgb(0, 83, 224), 1f);
+            Brush b = new LinearGradientBrush(gradientRect, Color.FromArgb(30, 250, 50), Color.FromArgb(130, 250, 150), 1f);
 
 
             g.FillRectangle(b, gradientRect);
         }
 
+        private void buttonDodajProcek_Click(object sender, EventArgs e)
+        {
+            int index = comboBoxProcesor.SelectedIndex;
+            pictureBoxProcesor.Image = procesory.ElementAt(index).zdjecie;
+        }
+
+        private void buttonDodajKarteGraf_Click(object sender, EventArgs e)
+        {
+            int index = comboBoxGrafa.SelectedIndex;
+            pictureBoxKartaGraf.Image = kartyGraficzne.ElementAt(index).zdjecie;
+        }
+
+        private void buttonDodajRam_Click(object sender, EventArgs e)
+        {
+            int index = comboBoxRAM.SelectedIndex;
+            pictureBoxRam.Image = ramy.ElementAt(index).zdjecie;
+        }
+
+        private void buttonDodajDysk_Click(object sender, EventArgs e)
+        {
+            int index = comboBoxDysk.SelectedIndex;
+            pictureBoxDysk.Image = dyski.ElementAt(index).zdjecie;
+        }
     }
 }
